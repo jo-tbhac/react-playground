6d29df3d32aa03234f92e256237032d8201ebd21
junpei ogawa <cornerstone.am3@gmail.com>
2024-04-28 17:03:53 +0900

    fix lint

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const conatiner = document.getElementById('root')
+
+if (conatiner) {
+  ReactDOM.createRoot(conatiner).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+}
