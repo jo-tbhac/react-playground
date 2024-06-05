83df470fa5a8009e14c0ac55c38ae32fbbaf00a0
junpei ogawa <cornerstone.am3@gmail.com>
2024-06-05 20:00:16 +0900

    setup react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import checker from 'vite-plugin-checker'
 
+const ReactCompilerConfig = {
+  compilationMode: 'annotation'
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]]
+      }
+    }),
     checker({
       typescript: true
     })
